316266b63ab4ed4c1932f3c5eca8afc2a0318ab8
gamithasam <gamitha@asia.com>
2024-11-06 23:12:17 +0530

    ProfileView design

--- park-mate/Sources/ParkMate/Views/ProfileView.swift
+++ park-mate/Sources/ParkMate/Views/ProfileView.swift
@@ -6,7 +6,46 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                List {
+                    VStack(spacing: 15) {
+                        ZStack {
+                            Rectangle()
+                                .frame(width: 55, height: 55)
+                                .foregroundColor(.gray)
+                                .cornerRadius(12)
+                            Image(systemName: "person.crop.circle")
+                                .resizable()
+                                .frame(width: 32, height: 32)
+                                .foregroundColor(.white)
+                        }
+                        
+                        Text("Profile")
+                            .font(.headline)
+                        
+                        Text("Manage your account details, vehicles, payment methods, and parking preferences.")
+                            .font(.body)
+                            .multilineTextAlignment(.center)
+                    }
+                    .padding(.vertical, 25)
+                    .padding(.horizontal, 20)
+                }
+                
+                List {
+                    NavigationLink("Personal information", destination: PersonalInfoView())
+                    NavigationLink("Vehicles", destination: destinationView)
+                    NavigationLink("Sign-In & Security", destination: destinationView)
+                }
+            }
+            .navigationTitle("Profile")
+//            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+    
+    @ViewBuilder
+    private func destinationView() -> some View {
+        Text("This is a placeholder.") // Placeholder view
     }
 }
 
